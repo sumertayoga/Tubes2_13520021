5766300139ac95ef667e7dc1013b054633fa8a24
averrous-s <averous009@gmail.com>
2022-03-14 11:20:03 +0700

    Add BFS

--- SearchAlgorithm/BFS.cs
+++ SearchAlgorithm/BFS.cs
@@ -3,14 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
+using System.IO;
 
 namespace SearchAlgorithm
 {
     public class BFS
     {
-        public static string Text()
+        private Queue<string> q;
+        private string[] visited;
+        private string initial;
+        private string destination;
+        public BFS(string initialDirectory, string destination)
         {
-            return "HALOOOO";
+            this.initial = initialDirectory;
+            this.destination = destination;
+            this.q = new Queue<string>();
+        }
+
+        public void crawl()
+        {
+            //inisialisasi
+            string accessed  = initial;
+            string[] subdirectories;
+            string queueAccessed;
+            visited.Append(initial);
+            q.Enqueue(initial);
+
+            while (q.Count != 0)
+            {
+                queueAccessed = q.Dequeue();
+                subdirectories = Directory.GetDirectories(queueAccessed);
+                for (int i = 0; i < subdirectories.Length; i++)
+                {
+                    accessed = subdirectories[i];
+                    if (! visited.Contains(accessed) )
+                    {
+                        visited.Append(accessed);
+                        q.Enqueue(accessed);
+                    }
+                }
+            }
         }
     }
 }
