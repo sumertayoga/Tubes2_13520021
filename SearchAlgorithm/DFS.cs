e0bd6660c8adc499ae8c10013d4c79d1561e3a2b
Jundan Haris <jundanha@gmail.com>
2022-03-20 09:32:47 +0700

    add dfs

--- SearchAlgorithm/DFS.cs
+++ SearchAlgorithm/DFS.cs
@@ -3,10 +3,75 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace SearchAlgorithm
 {
-    internal class DFS
+    public enum Mode
     {
+        First,
+        All
+    }
+
+    public class DFS
+    {
+        private Queue<string> queue;
+        private string initial;
+        private string destination;
+        private string[] visited;
+
+        public DFS(string initial, string destination)
+        {
+            this.queue = new Queue<string>();
+            this.initial = initial;
+            this.destination = destination;
+        }
+        
+        public void crawl(Mode mode)
+        {
+            //inisialisasi
+            visited = new string[] {};
+            Stack<string> stack = new Stack<string>();
+            string[] temp;
+            Boolean found = false;
+            string temp1;
+            string[] subdirectories;
+
+            //
+            stack.Push(initial);
+
+            //
+            while (stack.Count > 0 && !found)
+            {
+                //
+                temp1 = stack.Pop();
+
+                
+                // cek sudah pernah atau belum
+                if (!visited.Contains(temp1))
+                {
+                    //
+                    temp = temp1.Split('\\');
+                    if (mode == Mode.First && temp[temp.Length - 1] == destination)
+                    {
+                        found = true;
+                    } else if (temp[temp.Length - 1] == destination)
+                    {
+                        //nda tau ngapain
+                    }
+
+                    //
+                    visited.Append(temp1);
+
+                    //
+                    subdirectories = Directory.GetDirectories(temp1);
+                    for (int i = subdirectories.Length -1; i >= 0; i++)
+                    {
+                        stack.Push(subdirectories[i]);
+                    }
+                }
+
+            }
+        }  
     }
 }
